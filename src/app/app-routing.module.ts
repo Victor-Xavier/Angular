5e7bdfa1341b3c27beed070d2b1d295bb5ee7f8a
Técnico em Informática - 2019.3 <169121742019.3@senacrjedu.local>
2021-05-12 11:46:01 -0300

    aula 1 angula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SuspenseComponent } from './page/suspense/suspense.component';
+import { TerrorComponent } from './page/terror/terror.component';
+import { AventuraComponent } from './page/aventura/aventura.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'suspense', component:SuspenseComponent},
+  {path:'terror', component:TerrorComponent},
+  {path:'aventura', component:AventuraComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuspenseComponent } from './suspense/suspense.component';
+import { TerrorComponent } from './page/terror/terror.component';
+import { AventuraComponent } from './page/aventura/aventura.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuspenseComponent,
+    TerrorComponent,
+    AventuraComponent
   ],
   imports: [
     BrowserModule,
